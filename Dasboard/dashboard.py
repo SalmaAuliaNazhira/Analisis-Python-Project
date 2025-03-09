4261bce8a19150f7f288b1f9277d870c412f2cae
Salma Aulia Nazhira <127862908+SalmaAuliaNazhira@users.noreply.github.com>
2025-03-09 17:44:48 +0700

    Update dashboard.py

--- Dasboard/dashboard.py
+++ Dasboard/dashboard.py
@@ -7,8 +7,8 @@ import datetime
 sns.set(style='whitegrid')
 
 # Load dataset
-day_df = pd.read_csv("data/day.csv")
-hour_df = pd.read_csv("data/hour.csv")
+day_df = pd.read_csv("day.csv")
+hour_df = pd.read_csv("hour.csv")
 day_df["dteday"] = pd.to_datetime(day_df["dteday"])
 hour_df["dteday"] = pd.to_datetime(hour_df["dteday"])
 
